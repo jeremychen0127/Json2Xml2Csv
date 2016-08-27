891caed529c8d95b5606424fa5d9e13cc70e3ece
JeremyChen <jeremychen0127@gmail.com>
2016-08-27 18:45:13 -0400

    Fixed array bug

--- python/json2xml2csv/xml_to_json_utils.py
+++ python/json2xml2csv/xml_to_json_utils.py
@@ -11,7 +11,7 @@ def children_to_dict(xml_element):
     if children_dict.has_key(child_element.tag):
       existing_value = children_dict[child_element.tag]
 
-      if existing_value is ListType:
+      if type(existing_value) is ListType:
         children_dict[child_element.tag].append(new_value)
       else:
         children_dict[child_element.tag] = [existing_value, new_value]
