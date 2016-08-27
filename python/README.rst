cf7e8fc6638944c79441022828282c1a456928fb
JeremyChen <jeremychen0127@gmail.com>
2016-08-27 14:15:35 -0400

    Added python packaging information

---
+++ python/README.rst
@@ -0,0 +1,10 @@
+# Json2Xml2Csv
+
+Json2Xml2Csv is an utility for converting data files. Currently only support JSON to XML. The others are still being implemented
+
+### Execute as Converter
+After cloning or downloading Json2Xml2Csv, go into the project directory.
+
+Simply run `./json2xml SOURCE_JSON_FILE_PATH`
+
+The converted XML file will be in the same directory as this script
---
+++ python/setup.py
@@ -0,0 +1,74 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='json2xml2csv',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.0.dev1',
+
+    description='JSON/XML/CSV Converter',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/jeremychen0127/Json2Xml2Csv',
+
+    # Author details
+    author='Jeremy Chen',
+    author_email='jeremychen0127@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 2 - Pre-Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='JSON XML CSV convert converter conversion library',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=["json2xml2csv"],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'json2xml2csv=json2xml2csv.json_to_xml:main',
+        ],
+    },
+)
