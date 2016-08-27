e3436f878ef590ea0dbd1d8f418bd894ef60175c
JeremyChen <jeremychen0127@gmail.com>
2016-08-27 18:23:44 -0400

    Separated functions

--- python/json2xml2csv/xml_to_json.py
+++ python/json2xml2csv/xml_to_json.py
@@ -1,33 +1,9 @@
+import sys
+import time
+import json
 from lxml import etree
-from types import ListType
-from json_to_xml_utils import *
+from xml_to_json_utils import *
 from args import *
-
-def children_to_dict(xml_element):
-  children_dict = {}
-  for child_element in xml_element:
-    child_dict = element_to_dict(child_element)
-    new_value = child_dict[child_element.tag]
-    if children_dict.has_key(child_element.tag):
-      existing_value = children_dict[child_element.tag]
-      if existing_value is ListType:
-        children_dict[child_element.tag].append(new_value)
-      else:
-        children_dict[child_element.tag] = [existing_value, new_value]
-    else:
-      children_dict[child_element.tag] = new_value
-
-  return children_dict
-
-def element_to_dict(xml_element):
-  result_dict = {}
-  if len(xml_element):
-    children_dict = children_to_dict(xml_element)
-    result_dict[xml_element.tag] = children_dict
-  elif xml_element.text:
-    result_dict[xml_element.tag] = xml_element.text
-
-  return result_dict
 
 
 def xml_to_json(xml_element):
---
+++ python/json2xml2csv/xml_to_json_utils.py
@@ -0,0 +1,29 @@
+from lxml import etree
+from types import ListType
+
+
+def children_to_dict(xml_element):
+  children_dict = {}
+  for child_element in xml_element:
+    child_dict = element_to_dict(child_element)
+    new_value = child_dict[child_element.tag]
+    if children_dict.has_key(child_element.tag):
+      existing_value = children_dict[child_element.tag]
+      if existing_value is ListType:
+        children_dict[child_element.tag].append(new_value)
+      else:
+        children_dict[child_element.tag] = [existing_value, new_value]
+    else:
+      children_dict[child_element.tag] = new_value
+
+  return children_dict
+
+def element_to_dict(xml_element):
+  result_dict = {}
+  if len(xml_element):
+    children_dict = children_to_dict(xml_element)
+    result_dict[xml_element.tag] = children_dict
+  elif xml_element.text:
+    result_dict[xml_element.tag] = xml_element.text
+
+  return result_dict
